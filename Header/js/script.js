445c974e88adc4829334ed4656e08a54ea095fb4
NeonCoder23 <142743101+NeonCoder23@users.noreply.github.com>
2023-10-01 23:34:47 +0500

    Asosiy Portfolio
    Hozirda bosh sahifa mavjud, yangilanishlar kutilmoqda!

---
+++ Header/js/script.js
@@ -0,0 +1,57 @@
+// Setting Color Site
+document.querySelector(".settingClr").onclick = () => {
+
+    document.querySelector('.clrs').classList.toggle("block");
+    
+}
+
+
+function colorTxt() {
+    // Step 2: Add event listener to the color selector
+    const colorSelector = document.querySelectorAll('.clrs li');
+    let colorList = ['#00ff00','#f0ffff','#ffff00','#f00f00','#00ffff','#ffa500','#008080']
+    colorSelector.forEach((clrSelector, i) => {
+        clrSelector.addEventListener('click', function() {
+            // Step 3: Retrieve the selected color value
+            const selectedColor = colorList[i];
+        
+            // Step 4: Select elements with the "special-text" class
+            const specialTextElements = document.querySelectorAll('.clr');
+            specialTextElements.forEach(function(element) {
+                element.style.color = selectedColor;
+          });
+        });
+    })
+}
+
+// Aside open 
+function aside() {
+    const btn = document.querySelector('.menu');
+    const btnClosed = document.querySelector('.fa-close');
+    const navigation = document.querySelector('.aside');
+    const navigationItems = document.querySelectorAll('.aside a');
+
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('active')
+        navigation.classList.toggle('active')
+    })
+
+
+    navigationItems.forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            btn.classList.remove('active');
+            navigation.classList.remove('active');
+        })
+    })
+    btnClosed.addEventListener('click', () => {
+        navigation.classList.remove('active');
+    })
+}
+
+// Already All Funtions
+function startFunctions() {
+    // screenDarkMode()
+    aside()
+    colorTxt()
+}
+startFunctions()
